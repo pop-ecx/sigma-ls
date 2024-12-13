bb8a8e51a7d4719579eea7533072b281a2d2cc8e
pop-ecx <oliver.mulwa@protonmail.com>
2024-12-13 11:41:07 +0300

    feat: added completions for tags

--- features/completions.py
+++ features/completions.py
@@ -98,7 +98,13 @@ def register_completion_feature(server):
                 insert_text="CommandLine|contains:\n  - ${1:bash -c}\n  - ${2:iex}",
                 insert_text_format=InsertTextFormat.Snippet,
             ),
-            
+            CompletionItem(
+                label="Tags",
+                kind=CompletionItemKind.Keyword,
+                detail="Reference MITRE ATT&CK, CAR, TLP or CVE ",
+                insert_text="tags:\n  - ${1:attack.t1012}\n  - ${2:cve.2023-27997}",
+                insert_text_format=InsertTextFormat.Snippet,
+            ),
         ]
 
         return completions
