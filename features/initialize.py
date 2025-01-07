26aaa2583c68e0e501650a419c84ba81d5d2c429
pop-ecx <oliver.mulwa@protonmail.com>
2025-01-07 10:29:46 +0300

    register code actions feature

--- features/initialize.py
+++ features/initialize.py
@@ -1,13 +1,26 @@
 """
 Initialize deeznuts
 """
-from lsprotocol.types import InitializeParams, InitializeResult, TextDocumentSyncKind
+from lsprotocol.types import (
+    InitializeParams,
+    InitializeResult,
+    TextDocumentSyncKind,
+    CodeActionOptions,
+    CodeActionKind
+)
 
 #@server.feature("initialize")
 def initialize(params: InitializeParams) -> InitializeResult:
     """Initparams that returns initresults"""
     capabilities = {
         "textDocumentSync": TextDocumentSyncKind.Full,
-        "diagnosticProvider": True
+        "diagnosticProvider": True,
+        "codeActionProvider": CodeActionOptions(
+            code_action_kinds=[
+                CodeActionKind.QuickFix,
+                CodeActionKind.Refactor,
+                CodeActionKind.Source,
+            ]
+        ),
     }
     return InitializeResult(capabilities=capabilities)
